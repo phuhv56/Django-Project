6841f839c18503189387a07df7722078f963233d
phuhv56 <phuhv.56@gmail.com>
2014-06-18 18:45:50 +0700

    add static file
    initial static folder

--- footballsite/footballsite/settings.py
+++ footballsite/footballsite/settings.py
@@ -85,4 +85,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
+#STATIC_ROOT = 'C:/FirstGit/Django-Project/footballsite/static'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'apache')
+
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+    #put string here, like 'C:/...'
+)
