b592af3ed358dddfd85514c86561c04af0fb378b
phuhv56 <phuhv.56@gmail.com>
2014-06-18 11:00:13 +0700

    update urls view
    Update view urls models and make it excute

--- footballsite/club/urls.py
+++ footballsite/club/urls.py
@@ -4,7 +4,9 @@ from django.conf.urls import  patterns, include, url
 
 urlpatterns = patterns(
     '',
-    url(r'^simple/', 'club.views.simple_request'),
-    url(r'^template_re/', 'club.views.template_request'),
-    url(r'^temp_sim/', 'club.views.temp_simple'),
+    url(r'^simple/$', 'club.views.simple_request'),
+    url(r'^template_re/$', 'club.views.template_request'),
+    url(r'^temp_sim/$', 'club.views.temp_simple'),
+    url(r'^all/$', 'club.views.clubs'),
+    url(r'^get/(?P<club_id>\d+)/$', 'club.views.club'),
 )
--- footballsite/club/views.py
+++ footballsite/club/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import  render_to_response
 from django.template.loader import get_template
 from django.template import Context
 from django.http import HttpResponse
-
+from club.models import Club
 # Create your views here.
 def simple_request(request):
     name = 'Rich'
@@ -18,3 +18,7 @@ def template_request(request):
 def temp_simple(request):
     name = 'Phus'
     return render_to_response('exp.html', {'name': name})
+def clubs(request):
+    return render_to_response('clubs.html', {'clubs': Club.objects.all()})
+def club(request, club_id=1):
+    return render_to_response('club.html', {'club': Club.objects.get(id=club_id)})
